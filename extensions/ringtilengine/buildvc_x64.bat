d5ebba0ad097257e1797db5ed2ac7ecc13a583e1
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-08-31 22:48:21 +0300

    Update Package

--- extensions/ringtilengine/buildvc_x64.bat
+++ extensions/ringtilengine/buildvc_x64.bat
@@ -1,8 +1,8 @@
 cls
 setlocal enableextensions enabledelayedexpansion
-call ../../language/build/locatevc.bat x64
-cl /c %ringcflags% /DEBUG ring_tilengine.c -I "..\..\extensions\libdepwin\tilengine_x64\include" -I"..\..\language\include"
-link %ringldflags% ring_tilengine.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\tilengine_x64\lib\tilengine.lib /DLL /OUT:..\..\bin\ring_tilengine.dll
+call ../../language/build/locatevc.bat auto x64
+cl /c %ringcflags% ring_tilengine.c -I "..\..\extensions\libdepwin\tilengine_x64\include" -I"..\..\language\include"
+link %ringldflags% ring_tilengine.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\tilengine_x64\lib\tilengine.lib /DLL /OUT:..\..\bin\ring_tilengine.dll 
 del ring_tilengine.obj
 endlocal
 
